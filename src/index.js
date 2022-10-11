1aca6502bc3f3de7b1082452201ed8d96540666b
deukay <dk58554@gmail.com>
2022-10-11 17:07:53 +0300

    Added play command

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import { config } from "dotenv";
-import { Client } from "discord.js";
+import { Client, GatewayIntentBits, Routes } from "discord.js";
+import { REST } from "@discordjs/rest";
 
 config(); // Load .env
-const client = new Client({ intents: ["Guilds", "GuildMessages", "MessageContent"]});
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
+
+const client = new Client({ intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildIntegrations
+    ]
+});
+
+const rest = new REST({ version: '10'}).setToken(BOT_TOKEN);
 
 client.on("ready", () => {
     console.log(`${client.user.tag} is running`);
@@ -16,4 +30,39 @@ client.on("messageCreate", (message) => {
     }
 });
 
-client.login(process.env.BOT_TOKEN);
+client.on("interactionCreate", (interaction) => {
+    if (interaction.isChatInputCommand()) {
+        const song = interaction.options.get("song").value;
+        console.log("Playing: " + song);
+        interaction.reply({ content: `Playing: ${song}`});
+    }
+});
+
+async function main() {
+    const commands = [
+        {
+            name: "play",
+            description: "Play music command",
+            options: [
+                {
+                    name: "song",
+                    description: "Song to play",
+                    type: 3,
+                    required: true
+                },
+            ]
+        }
+    ];
+    try {
+        console.log("Started refreshing application (/) commands.");
+        await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+            body: commands,
+        });
+
+        client.login(BOT_TOKEN);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+main();
